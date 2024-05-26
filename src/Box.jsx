ba34f69097c440d91fd647826978f35a54fb5c97
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2024-05-26 10:14:55 -0400

    Initial commit

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,25 +1,38 @@
-import { useRef } from 'react'
-import { useFrame } from '@react-three/fiber'
+import {useRef} from 'react';
+import {useFrame} from '@react-three/fiber';
 
 export default function Box(props) {
-  const ref = useRef()
+  const ref = useRef();
 
   useFrame((_, delta) => {
-    ref.current.rotation.x += 1 * delta
-    ref.current.rotation.y += 0.5 * delta
-  })
+    ref.current.rotation.x += 1 * delta;
+    ref.current.rotation.y += 0.5 * delta;
+  });
+
+  const onPointerOver = (e) => {
+    console.log('pointer over ' + e.object.name);
+  };
+
+  const onPointerOut = (e) => {
+    console.log('pointer out ' + e.object.name);
+  };
+
+  const onPointerDown = (e) => {
+    console.log('pointer down ' + e.object.name);
+  };
+
+  const onPointerUp = (e) => {
+    console.log('pointer up ' + e.object.name);
+  };
+
+  const onUpdate = (self) => {
+    console.log('onUpdate', self);
+  };
 
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      onPointerDown={(e) => console.log('pointer down ' + e.object.name)}
-      onPointerUp={(e) => console.log('pointer up ' + e.object.name)}
-      onPointerOver={(e) => console.log('pointer over ' + e.object.name)}
-      onPointerOut={(e) => console.log('pointer out ' + e.object.name)}
-      onUpdate={(self) => console.log(self)}>
+    <mesh {...props} ref={ref} onPointerOver={onPointerOver} onPointerOut={onPointerOut} onPointerDown={onPointerDown} onPointerUp={onPointerUp} onUpdate={onUpdate}>
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
-  )
+  );
 }
