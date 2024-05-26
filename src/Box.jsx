c07fe6018616dae80b5abb4bc64b5c4b3cc3098f
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2024-05-26 12:35:58 -0400

    Initial commit

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,31 +1,26 @@
-import { useRef, useState, useEffect, useMemo } from 'react'
-import { useFrame } from '@react-three/fiber'
-import * as THREE from 'three'
+import {useRef, useState, useEffect, useMemo} from 'react';
+import {useFrame} from '@react-three/fiber';
+import {BoxGeometry, SphereGeometry} from 'three';
 
 export default function Box(props) {
-  const ref = useRef()
-  const [count, setCount] = useState(0)
-  const geometry = useMemo(
-    () => [new THREE.BoxGeometry(), new THREE.SphereGeometry(0.785398)],
-    []
-  )
+  const ref = useRef();
+  const [count, setCount] = useState(0);
+  const geometry = useMemo(() => [new BoxGeometry(), new SphereGeometry(0.785398)], []);
 
   useEffect(() => {
-    console.log(ref.current.geometry.uuid)
-  })
+    console.log(ref.current.geometry.uuid);
+  });
 
   useFrame((_, delta) => {
-    ref.current.rotation.x += delta
-    ref.current.rotation.y += 0.5 * delta
-  })
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += 0.5 * delta;
+  });
+
+  const onPointerDown = () => setCount((count + 1) % 2);
 
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      onPointerDown={() => setCount((count + 1) % 2)}
-      geometry={geometry[count]}>
+    <mesh {...props} ref={ref} geometry={geometry[count]} onPointerDown={onPointerDown}>
       <meshBasicMaterial color={'lime'} wireframe />
     </mesh>
-  )
+  );
 }
