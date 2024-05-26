917b9e2d71cf6e6d31d03b59e1a0032a4e04933b
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2024-05-26 09:21:33 -0400

    Initial commit

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,8 +1,9 @@
 export default function Box(props) {
+  console.log(props);
   return (
     <mesh {...props}>
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
-  )
+  );
 }
