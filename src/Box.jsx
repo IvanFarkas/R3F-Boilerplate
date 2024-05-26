e6c974bff247a6456e10b5ad48920b2ea70bfe94
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2024-05-26 09:45:53 -0400

    Initial commit

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,13 +1,20 @@
-import { useRef } from 'react'
+import {useEffect, useRef} from 'react';
 
 export default function Box(props) {
-  const ref = useRef()
-  console.log(ref)
+  const ref = useRef();
+
+  useEffect(() => {
+    const currentRef = ref.current;
+
+    if (currentRef != undefined) {
+      console.log(currentRef);
+    }
+  });
 
   return (
     <mesh {...props} ref={ref}>
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
-  )
+  );
 }
