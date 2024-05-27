cea2713e3651a8dd3ee496ed292caea722c6d444
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2024-05-27 07:17:15 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { Canvas } from '@react-three/fiber'
-import Polyhedron from './Polyhedron'
-import * as THREE from 'three'
-import { Stats, OrbitControls } from '@react-three/drei'
+import {Canvas} from '@react-three/fiber';
+import {Stats, OrbitControls} from '@react-three/drei';
+import * as THREE from 'three';
+import Polyhedron from './Polyhedron';
 
 export default function App() {
   const polyhedron = [
     new THREE.BoxGeometry(),
     new THREE.SphereGeometry(0.785398),
-    new THREE.DodecahedronGeometry(0.785398)
-  ]
+    new THREE.DodecahedronGeometry(0.785398),
+  ];
 
   return (
-    <Canvas camera={{ position: [0, 0, 3] }}>
+    <Canvas camera={{position: [0, 0, 3]}}>
       <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
@@ -19,5 +19,5 @@ export default function App() {
       <OrbitControls />
       <Stats />
     </Canvas>
-  )
+  );
 }
--- src/Polyhedron.jsx
+++ src/Polyhedron.jsx
@@ -1,26 +1,22 @@
-import { useRef, useState } from 'react'
-import { useFrame } from '@react-three/fiber'
+import {useRef, useState} from 'react';
+import {useFrame} from '@react-three/fiber';
 
-export default function Polyhedron({ position, polyhedron }) {
-  const ref = useRef()
-  const [count, setCount] = useState(0)
+export default function Polyhedron({position, polyhedron}) {
+  const ref = useRef();
+  const [count, setCount] = useState(0);
 
-  console.log(polyhedron)
+  console.log(polyhedron);
 
   useFrame((_, delta) => {
-    ref.current.rotation.x += delta
-    ref.current.rotation.y += 0.5 * delta
-  })
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += 0.5 * delta;
+  });
+
+  const onPointerDown = () => setCount((count + 1) % 3);
 
   return (
-    <mesh
-      position={position}
-      ref={ref}
-      onPointerDown={() => {
-        setCount((count + 1) % 3)
-      }}
-      geometry={polyhedron[count]}>
+    <mesh position={position} ref={ref} onPointerDown={onPointerDown} geometry={polyhedron[count]}>
       <meshBasicMaterial color={'lime'} wireframe />
     </mesh>
-  )
+  );
 }
