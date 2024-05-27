1966832d19795a1822f37c091899ced18143cd0b
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2024-05-27 08:22:22 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,37 @@
-import { Canvas } from '@react-three/fiber'
-import Polyhedron from './Polyhedron'
-import * as THREE from 'three'
-import { useMemo } from 'react'
-import { Stats, OrbitControls } from '@react-three/drei'
-import { useControls } from 'leva'
+// Leva - https://github.com/pmndrs/leva
+
+import {useMemo} from 'react';
+import {Canvas} from '@react-three/fiber';
+import {Stats, OrbitControls} from '@react-three/drei';
+import * as THREE from 'three';
+import {useControls} from 'leva';
+import Polyhedron from './Polyhedron';
 
 export default function App() {
-  const polyhedron = useMemo(
-    () => [new THREE.BoxGeometry(), new THREE.SphereGeometry(0.785398), new THREE.DodecahedronGeometry(0.785398)],
-    []
-  )
+  const polyhedron = useMemo(() => [new THREE.BoxGeometry(), new THREE.SphereGeometry(0.785398), new THREE.DodecahedronGeometry(0.785398)], []);
 
   const options = useMemo(() => {
     return {
-      x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+      // Input Types - https://github.com/pmndrs/leva/blob/main/docs/inputs.md#input-types
+      x: {value: 0, min: 0, max: Math.PI * 2, step: 0.01},
+      y: {value: 0, min: 0, max: Math.PI * 2, step: 0.01},
+      z: {value: 0, min: 0, max: Math.PI * 2, step: 0.01},
       visible: true,
-      color: { value: 'lime' }
-    }
-  }, [])
+      color: {value: 'lime'},
+    };
+  }, []);
 
-  const pA = useControls('Polyhedron A', options)
-  const pB = useControls('Polyhedron B', options)
+  const pA = useControls('Polyhedron A', options);
+  const pB = useControls('Polyhedron B', options);
 
   return (
-    <Canvas camera={{ position: [1, 2, 3] }}>
-      <Polyhedron
-        position={[-1, 1, 0]}
-        rotation={[pA.x, pA.y, pA.z]}
-        visible={pA.visible}
-        color={pA.color}
-        polyhedron={polyhedron}
-      />
-      <Polyhedron
-        position={[1, 1, 0]}
-        rotation={[pB.x, pB.y, pB.z]}
-        visible={pB.visible}
-        color={pB.color}
-        polyhedron={polyhedron}
-      />
+    <Canvas camera={{position: [1, 2, 3]}}>
+      <Polyhedron position={[-1, 1, 0]} rotation={[pA.x, pA.y, pA.z]} visible={pA.visible} color={pA.color} polyhedron={polyhedron} />
+      <Polyhedron position={[1, 1, 0]} rotation={[pB.x, pB.y, pB.z]} visible={pB.visible} color={pB.color} polyhedron={polyhedron} />
       <OrbitControls target-y={1} />
       <axesHelper args={[5]} />
       <gridHelper />
       <Stats />
     </Canvas>
-  )
+  );
 }
--- src/Polyhedron.jsx
+++ src/Polyhedron.jsx
@@ -1,20 +1,16 @@
-import { useRef, useState } from 'react'
+import {useRef, useState} from 'react';
 
-export default function Polyhedron({ polyhedron, color, ...props }) {
-  const ref = useRef()
-  const [count, setCount] = useState(2)
+export default function Polyhedron({polyhedron, color, ...props}) {
+  const ref = useRef();
+  const [count, setCount] = useState(2);
 
-  console.log(polyhedron[count].uuid)
+  console.log(polyhedron[count].uuid);
+
+  const onPointerDown = () => setCount((count + 1) % 3);
 
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      onPointerDown={() => {
-        setCount((count + 1) % 3)
-      }}
-      geometry={polyhedron[count]}>
+    <mesh {...props} ref={ref} onPointerDown={onPointerDown} geometry={polyhedron[count]}>
       <meshBasicMaterial color={color} wireframe />
     </mesh>
-  )
+  );
 }
