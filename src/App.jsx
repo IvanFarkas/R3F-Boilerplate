0efdb5396788723410d04d3fc1c38829f2bdb479
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2024-05-27 15:03:07 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { Stats, OrbitControls, Circle } from '@react-three/drei'
-import { Canvas, useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
+import {Canvas, useLoader} from '@react-three/fiber';
+import {Stats, OrbitControls, Circle} from '@react-three/drei';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader';
 
-export default function App() {
-  const gltf = useLoader(GLTFLoader, '/models/monkey.glb')
+const App = () => {
+  const gltf = useLoader(GLTFLoader, '/models/monkey.glb');
 
   return (
-    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+    <Canvas camera={{position: [-0.5, 1, 2]}} shadows>
       <directionalLight position={[3.3, 1.0, 4.4]} intensity={Math.PI} castShadow />
-      <primitive
-        object={gltf.scene}
-        position={[0, 1, 0]}
-        children-0-castShadow
-      />
+      <primitive object={gltf.scene} position={[0, 1, 0]} children-0-castShadow />
       <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
         <meshStandardMaterial />
       </Circle>
@@ -20,5 +16,7 @@ export default function App() {
       <axesHelper args={[5]} />
       <Stats />
     </Canvas>
-  )
-}
+  );
+};
+
+export default App;
